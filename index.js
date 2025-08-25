e0a52d16e06ccf83e1fc3a8b783b9fbe32d1731c
ugo635 <ugolechien58901@gmail.com>
2025-08-25 21:38:45 +0200

    Update

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ import "./features/Stash.js";
 import "./features/FallingBlocks.js";
 import "./features/Mining.js";
 import "./features/Party.js";
-import "./features/ColorReplacor.js";
+import "./features/colorReplacor.js";
 import "./features/Warps.js";
 import "./features/Potions.js";
 import "./features/viewAuctions.js";
 import "./features/Features.js";
+import "./features/mutedChat.js";
 
 register("gameLoad", () => ChatLib.chat("&6&l[Cores Module] &r&7Module Loaded"));
 

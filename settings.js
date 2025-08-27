ce99ae9b92c2862980b8edc3d1b3380e6463a0df
ugo635 <ugolechien58901@gmail.com>
2025-08-27 19:52:26 +0200

    Fix Chroma

--- settings.js
+++ settings.js
@@ -71,7 +71,7 @@ class cmSettingsData {
         description: "Choose color username (/color for example)",
         category: "Color Replacor",
         subcategory: "Settings",
-        options: ["Black", "Dark Blue", "Dark Green", "Dark Aqua", "Dark Red", "Dark Purple", "Gold", "Gray", "Dark Gray", "Blue", "Green", "Aqua", "Red", "Light Purple / Pink", "Yellow", "Chroma"]
+        options: ["Black", "Dark Blue", "Dark Green", "Dark Aqua", "Dark Red", "Dark Purple", "Gold", "Gray", "Dark Gray", "Blue", "Green", "Aqua", "Red", "Light Purple / Pink", "Yellow", "White", "Chroma"]
     })
     colorUser = 0;
     @SwitchProperty({
@@ -101,7 +101,7 @@ class cmSettingsData {
         description: "Choose color tag (/color for example)",
         category: "Color Replacor",
         subcategory: "Settings",
-        options: ["Black", "Dark Blue", "Dark Green", "Dark Aqua", "Dark Red", "Dark Purple", "Gold", "Gray", "Dark Gray", "Blue", "Green", "Aqua", "Red", "Light Purple / Pink", "Yellow", "Chroma"]
+        options: ["Black", "Dark Blue", "Dark Green", "Dark Aqua", "Dark Red", "Dark Purple", "Gold", "Gray", "Dark Gray", "Blue", "Green", "Aqua", "Red", "Light Purple / Pink", "Yellow", "White", "Chroma"]
     })
     colorTag = 0;
     @SwitchProperty({
